43ebb2921ab53a58dd075d614d086118d5638d79
nwespe <nicholewespe@gmail.com>
2017-11-06 11:55:07 -0500

    Added handling of negative integers.

---
+++ DC5 Sorted CS Coding/sorted_challenge.py
@@ -0,0 +1,91 @@
+# coding: utf-8
+
+import sys
+
+"""
+Please write a program that takes a list of strings containing
+integers and words and returns a sorted version of the list.
+The goal is to sort this list in such a way that all words are in
+alphabetical order and all integers are in numerical order.
+Furthermore, if the nth element in the list is an integer it must
+remain an integer, and if it is a word it must remain a word.
+
+In addition, the strings and integers may contain characters that are
+ascii symbols that neither belong to letter set nor digit set (i.e. "#", "%", ";", etc).
+You are required to remove them during the process so that the output
+will contain only letters or digits. For example, if a string is "sym*bo+l",
+the output should be "symbol". If an integer is "12%3", the output should be "123".
+You don't have to worry about strings or integers that contain only non­letter­non­digit characters,
+like "^!?", "&", etc.
+
+For example: 20 cat bi?rd 12 do@g >> should read >> 12 bird cat 20 dog
+"""
+
+
+def read_input(input_file):
+    # read single line from file into a list of strings
+    with open(input_file, 'r') as f:
+        readfile = f.read().split(' ')
+    return readfile
+
+
+def clean_symbols(int_word_list):
+    # remove any non-alphanumeric symbols from the ints and words in the list
+    for i, x in enumerate(int_word_list):
+        if not x.isalnum():
+            if x[0] == '-':   # special handling for negative integers
+                cleaned_x = [a for a in x[1:] if a.isalnum()]
+                if ''.join(cleaned_x).isdigit():  # assume negative integer if first symbol is '-' followed by digits
+                    cleaned_x = ['-'] + cleaned_x
+            else:
+                cleaned_x = [a for a in x if a.isalnum()]
+            int_word_list[i] = ''.join(cleaned_x)
+    return int_word_list
+
+
+def separate_ints_words(int_word_list):
+    # separate into two lists, record positions of ints vs. words in original list
+    int_list = []
+    word_list = []
+    indices = []
+    for x in int_word_list:
+        if x.isdigit():
+            int_list.append(int(x))
+            indices.append(0)
+        elif x[0] == '-' and x[1:].isdigit():  # special handling of negative numbers
+            int_list.append(int(x))
+            indices.append(0)
+        else:
+            word_list.append(x)
+            indices.append(1)
+    return int_list, word_list, indices
+
+
+def sort_lists(int_list, word_list):
+    # use reverse order to use .pop() later
+    return sorted(int_list, reverse=True), sorted(word_list, reverse=True)
+
+
+def reassemble_list(int_list, word_list, indices):
+    # recombine sorted lists using position information from original list
+    sorted_joined_list = []
+    for x in indices:
+        if x == 0:
+            sorted_joined_list.append(int_list.pop())
+        else:
+            sorted_joined_list.append(word_list.pop())
+    return sorted_joined_list
+
+
+def main(input_file):
+    print 'received input file ' + input_file
+    readfile = read_input(input_file)
+    cleaned_list = clean_symbols(readfile)
+    int_list, word_list, indices = separate_ints_words(cleaned_list)
+    sorted_ints, sorted_words = sort_lists(int_list, word_list)
+    print reassemble_list(sorted_ints, sorted_words, indices)
+
+
+if __name__ == '__main__':
+    input_file = sys.argv[1]
+    main(input_file)
